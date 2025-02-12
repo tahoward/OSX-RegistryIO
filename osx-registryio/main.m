c757847974e816f0804326eaf01900ad37117ead
thoward-godaddy <thoward@godaddy.com>
2025-02-12 11:44:20 -0700

    Initial Commit

---
+++ osx-registryio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  osx-registryio
+//
+//  Created by Trevor Howard on 2/12/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
